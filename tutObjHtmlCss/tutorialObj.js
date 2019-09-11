9989061357741d620e414a31e654f7000211dae6
Mikhail <Egorkin.Mikhail@gmail.com>
2019-09-11 17:17:10 +0300

    lectionsObj

---
+++ tutObjHtmlCss/tutorialObj.js
@@ -0,0 +1,19 @@
+let box = document.getElementById('box');
+
+let btn = document.getElementsByTagName('button'),
+    crcl = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
+
+console.log(box);
+
+console.log(btn);
+console.log(btn[0]);
+
+console.log(crcl);
+console.log(crcl[2]);
+
+console.log(heart);
+console.log(heart[1]);
+
+console.log(oneHeart);
