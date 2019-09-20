98593a5545b89e5120d8cd7bf5884bf2c8fb87bc
Mikhail <Egorkin.Mikhail@gmail.com>
2019-09-20 16:25:02 +0300

    lesson 8 Yoga

--- tutorial8Yoga/js/script.js
+++ tutorial8Yoga/js/script.js
@@ -35,4 +35,49 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+
+    let deadLine = '2019-09-21';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        //console.log(t);
+        let hh = Math.floor(t/(1000*60*60)),
+            mm = Math.floor((t/1000/60) % 60),
+            ss = Math.floor((t/1000) % 60);
+            ss = (ss > 9) ? ss : "0"+ss;
+            mm = (mm > 9) ? mm : "0"+mm;
+            hh = (hh > 9) ? hh : "0"+hh;
+        return {
+            'total' : t,
+            'hh' : hh,
+            'mm' : mm,
+            'ss' : ss
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hh = timer.querySelector('.hours'),
+            mm = timer.querySelector('.minutes'),
+            ss = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+            function updateClock() {
+                let t = getTimeRemaining(endTime);
+                hh.textContent = t.hh;
+                mm.textContent = t.mm;
+                ss.textContent = t.ss;                
+            
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hh.textContent = "00";
+                mm.textContent = "00";
+                ss.textContent = "00";
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
